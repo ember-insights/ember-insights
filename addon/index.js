af8314c180cf402ab550d54551fb78370f267684
Zatvor <a.zatvornitskiy@gmail.com>
2015-01-16 14:10:34 +0200

    Simplified Addon entity

--- addon/index.js
+++ addon/index.js
@@ -4,19 +4,16 @@ import runtime    from './runtime';
 import middleware from './middleware';
 
 var initializer = (function() {
-  var Addon = new (function() { // jshint ignore:line
-
-    this.isActivated  = false;
-    this.configs      = {};
-    this.settings     = null;
-
-  })();
+  var Addon = {
+    isActivated:  false,
+    configs:      {},
+    settings:     null
+  };
 
   // start catching all of actions and transitions
   middleware.use(Addon);
 
   return runtime(Addon);
-
 })();
 
 export default initializer;

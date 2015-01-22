f6a0b3086b165df084fbc72110d5c41d8e12d8dc
Zatvor <a.zatvornitskiy@gmail.com>
2015-01-22 19:30:00 +0200

    Tiny improvements

--- addon/handlers.js
+++ addon/handlers.js
@@ -2,19 +2,16 @@ var main = {
 
   handler: function(settings) {
     return function(type, data, tracker) {
-      if (type === 'transition') {
-        main.transitionHandler(data, tracker, {
-          trackTransitionsAs: settings.trackTransitionsAs
-        });
-      }
-      if (type === 'action') {
-        main.actionHandler(data, tracker);
-      }
+      if (type === 'transition')
+        main.transitionHandler(data, tracker, settings);
+
+      if (type === 'action')
+        main.actionHandler(data, tracker, settings);
     };
   },
 
-  transitionHandler: function(data, tracker, options) {
-    var trackType = options.trackTransitionsAs;
+  transitionHandler: function(data, tracker, settings) {
+    var trackType = settings.trackTransitionsAs;
 
     if (trackType === 'event'    || trackType === 'both') {
       tracker.sendEvent(
@@ -27,17 +24,15 @@ var main = {
     }
   },
 
-  actionHandler: function(data, tracker) {
+  actionHandler: function(data, tracker, settings) {
     var args = ['ember_action', data.actionName];
-
     var actionLabel = data.actionArguments[0],
     actionValue = data.actionArguments[1];
 
     if (actionLabel != null) {
       args[2] = actionLabel;
-      if (actionValue != null) {
+      if (actionValue != null)
         args[3] = actionValue;
-      }
     }
 
     tracker.sendEvent.apply(tracker, args);

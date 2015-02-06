76455e79d91a6c72751bc2c4b578880335200570
Alexander Obozinsky <a.obozinsky@roundscope.com>
2015-02-06 17:09:31 +0200

    mocha handlers tests #42

---
+++ tests/unit/handlers/main-action-test.js
@@ -0,0 +1,61 @@
+import DefaultHandler from 'ember-insights/handler';
+import { it } from 'ember-mocha';
+
+
+describe('Main handler for matched actions',function(){
+  var handler = DefaultHandler.actionHandler;
+
+  it('tests Action without label and value', function(done) {
+    var data = {
+          actionName: 'btnClick',
+          actionArguments: []
+        },
+        tracker = {
+          sendEvent: function(category, action, label, value) {
+            expect(arguments.length).to.equal(2);
+            expect(category).to.equal('ember_action');
+            expect(action).to.equal('btnClick');
+            done();
+          }
+        };
+
+    handler(data, tracker);
+  });
+
+  it('tests Action with label', function(done) {
+    var data = {
+          actionName: 'btnClick',
+          actionArguments: ['aLabel']
+        },
+        tracker = {
+          sendEvent: function(category, action, label, value) {
+            expect(arguments.length).to.equal(3);
+            expect(category).to.equal('ember_action');
+            expect(action).to.equal('btnClick');
+            expect(label).to.equal('aLabel');
+            done();
+          }
+        };
+
+    handler(data, tracker);
+  });
+
+  it('tests Action with label and value', function(done) {
+    var data = {
+          actionName: 'btnClick',
+          actionArguments: ['aLabel', 'aValue']
+        },
+        tracker = {
+          sendEvent: function(category, action, label, value) {
+            expect(arguments.length).to.equal(4);
+            expect(category).to.equal('ember_action');
+            expect(action).to.equal('btnClick');
+            expect(label).to.equal('aLabel');
+            expect(value).to.equal('aValue');
+            done();
+          }
+        };
+
+    handler(data, tracker);
+  });
+});
---
+++ tests/unit/handlers/main-transition-test.js
@@ -0,0 +1,87 @@
+import DefaultHandler from 'ember-insights/handler';
+import { it } from 'ember-mocha';
+
+
+describe('Main handler for matched transitions',function(){
+  var handler = DefaultHandler.transitionHandler;
+
+  it('tests Transition as event', function() {
+    var settings = { trackTransitionsAs: 'event'},
+        data = {
+          oldRouteName: 'outer.inner.nested',
+          routeName: 'outer.inner.index',
+        },
+        tracker = {
+          sendEvent: function(type, json) {
+            var parsed = JSON.parse(json);
+            expect(type).to.equal('ember_transition');
+            expect(parsed.from).to.equal('outer.inner.nested');
+            expect(parsed.to).to.equal('outer.inner.index');
+          }
+        };
+    handler(data, tracker, settings);
+  });
+
+  it('tests Transition as pageview', function() {
+    var settings = { trackTransitionsAs: 'pageview'},
+        data = { url: '/outer/inner' },
+        tracker = {
+          trackPageView: function(url) {
+            expect(url).to.equal('/outer/inner');
+          }
+        };
+
+    handler(data, tracker, settings);
+  });
+
+  describe('Transitions handling functions called right by tracking type',function(){
+    var sendEventCalled;
+    var trackPageViewCalled;
+
+    beforeEach(function(){
+      sendEventCalled = false;
+      trackPageViewCalled = false;
+    });
+
+    it('Transition only as event calls only sendEvent', function() {
+      var settings = { trackTransitionsAs: 'event'},
+          tracker = {
+            sendEvent: function() {
+              sendEventCalled = true;
+            },
+            trackPageView: function() {
+              trackPageViewCalled = true;
+            }
+          };
+      handler({}, tracker, settings);
+      expect(sendEventCalled).to.be.ok();
+      expect(trackPageViewCalled).not.to.be.ok();
+    });
+
+    it('Transition only as pageview calls only trackPageView', function() {
+      var settings = { trackTransitionsAs: 'pageview'},
+          tracker = {
+            sendEvent: function() {
+              sendEventCalled = true;
+            },
+            trackPageView: function() {
+              trackPageViewCalled = true;
+            }
+          };
+      handler({}, tracker, settings);
+      expect(sendEventCalled).not.to.be.ok();
+      expect(trackPageViewCalled).to.be.ok();
+    });
+
+    it('Transition both as pageview and as event calls both functions', function() {
+      var settings = { trackTransitionsAs: 'both'},
+          tracker = {
+            sendEvent:     function() { sendEventCalled = true; },
+            trackPageView: function() { trackPageViewCalled = true; }
+          };
+      handler({}, tracker, settings);
+      expect(sendEventCalled).to.be.ok();
+      expect(trackPageViewCalled).to.be.ok();
+    });
+  });
+});

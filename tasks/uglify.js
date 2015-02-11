2091094fd82e614ff7466d36e5acad840c100fe2
Alexander Obozinsky <a.obozinsky@gmail.com>
2015-02-11 15:21:41 +0200

    grunt added for amd build purpose #72

---
+++ tasks/uglify.js
@@ -0,0 +1,16 @@
+var grunt = require('grunt');
+
+module.exports = {
+  options: {
+    report: 'min',
+    wrap: true
+  },
+  dist: {
+    files: [
+      {
+        src: 'dist/ember-insights.amd.js',
+        dest: 'dist/ember-insights.amd.min.js',
+      },
+    ],
+  },
+};

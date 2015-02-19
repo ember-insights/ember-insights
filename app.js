6aca54c1728eef3a5a5f157a64d1af6ddbc98399
Zatvor <a.zatvornitskiy@gmail.com>
2015-02-19 18:02:22 +0200

    Example app: Task Execution (in general), #46

--- app.js
+++ app.js
@@ -1,65 +1,49 @@
 App = Ember.Application.create({});
 
 App.Router.map(function() {
-  this.resource('index', { path: '/' });
+  this.resource('index',     { path: '/' });
   this.resource('execution', { path: '/execution' });
-  this.resource('result', { path: '/result' });
+  this.resource('result',    { path: '/result' });
+});
+
+App.Task = Ember.Object.extend({
+  validateTask:  Ember.computed.equal('taskID', 'Mnemonics'),
+  validateRadio: Ember.computed.equal('selectedRadio', 'should'),
+  isValid:       Ember.computed.and('validateTask', 'validateRadio', 'checkedOptions.s', 'checkedOptions.l'),
+  isFails:       Ember.computed.gt('attempts',1)
 });
 
 App.ExecutionRoute = Ember.Route.extend({
   model: function(){
-    var modelClass = Ember.Object.extend({
-      validateTask: Ember.computed.equal('taskID', 'Mnemonics'),
-      validateRadio: Ember.computed.equal('selectedRadio', 'should'),
-      valid: Ember.computed.and('validateTask', 'validateRadio', 'checkedOptions.s', 'checkedOptions.l'),
-      failsDetected: Ember.computed.gt('attempts',1)
-    });
-
-    return modelClass.create({
-      attempts: 0,
-      taskID: '',
-      selectedRadio: 'must',
-      checkedOptions: {
-        s:false,
-        o:false,
-        l:false,
-        i:false,
-        d:false
-      }
+    return App.Task.create({
+      attempts:       0,
+      taskID:         '',
+      selectedRadio:  'must',
+      checkedOptions: { s:false, o:false, l:false, i:false, d:false }
     });
   }
 });
 
 App.ResultRoute = Ember.Route.extend({
-  model: function(){
-    return this.modelFor('execution');
-  }
+  model: function(){ return this.modelFor('execution'); }
 });
 
 
 App.IndexController = Ember.Controller.extend({
   actions: {
-    next: function(){
-      this.transitionTo('execution');
-    }
+    next: function(){ this.transitionTo('execution'); }
   }
 });
 
 App.ExecutionController = Ember.ObjectController.extend({
-
-  submitted: false,
-  radioButtons: {
-      must: 'must',
-      should: 'should',
-      could: 'could',
-      wont: 'wont'
-    },
+  submitted:    false,
+  radioButtons: { must: 'must', should: 'should', could: 'could', wont: 'wont' },
 
   actions: {
     submit: function(){
       this.incrementProperty('model.attempts');
 
-      if(this.get('model.valid') || this.get('model.attempts') > 1){
+      if(this.get('model.isValid') || this.get('model.attempts') > 1){
         this.transitionTo('result');
       }
     }
@@ -67,20 +51,15 @@ App.ExecutionController = Ember.ObjectController.extend({
 });
 
 App.ResultController = Ember.Controller.extend({
-
   actions: {
-    restart: function(){
-      this.transitionTo('index');
-    }
+    restart: function(){ this.transitionTo('index'); }
   }
-
-
 });
 
 App.RadioButtonComponent = Ember.Component.extend({
-  tagName: 'input',
-  type: 'radio',
-  attributeBindings: [ 'disabled', 'checked', 'name', 'type', 'value' ],
+  tagName:            'input',
+  type:               'radio',
+  attributeBindings:  [ 'disabled', 'checked', 'name', 'type', 'value' ],
 
   checked: function () {
     return this.get('value') === this.get('selectedRadio');

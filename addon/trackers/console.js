032e5f233aed45a814a98b3a41c5ac3ff836bc00
Zatvor <a.zatvornitskiy@gmail.com>
2015-02-20 19:54:27 +0200

    Cleanup ConsoleTracker, #67

--- addon/trackers/console.js
+++ addon/trackers/console.js
@@ -2,85 +2,34 @@
 
 import AbstractTracker from './abstract-tracker';
 
-function trackerFun() {
-  return function(){
-    console.log('Ember-Insights event');
-    console.log('\tAction: ', arguments[0]);
-    console.log('\tAction argument: ', arguments[1]);
-    if(arguments.length > 2){
-      console.log('\tAdditional arguments: ');
-      for(var i=2; i < arguments.length; i++){
-         console.log('\t\t', arguments[i]);
-      }
-    }
-  };
+function logger(label, params) {
+  var message = 'EmberInsights.ConsoleTracker.%@(%@)'.fmt(label, params);
+  Ember.Logger.log(message);
 }
-
-function trackingNamespace(name) {
-  return function(action) {
-    return (name ? name + '.' : '') + action;
-  };
-}
-
 
 export default {
-  factory: function(settings) {
-
-    var tracker   = trackerFun(settings.trackerFun);
-    var namespace = trackingNamespace(settings.trackingNamespace);
-
-    // Runtime conveniences as a wrapper for tracker function
+  factory: function() {
     var Tracker = AbstractTracker.extend({
-      init: function() {
-      },
-
-      isTracker: function() {
-        return (tracker && typeof tracker === 'function');
-      },
       getTracker: function() {
-        if (! this.isTracker()) {
-          Ember.debug("Can't find in `window` a `" + settings.trackerFun + "` function definition");
-        }
-        return tracker;
+        return logger;
       },
       set: function(key, value) {
-        tracker(namespace('set'), key, value);
+        logger('set', [key, value]);
       },
       send: function(fieldNameObj) {
-        fieldNameObj = fieldNameObj || {};
-        tracker(namespace('send'), fieldNameObj);
+        logger('send', [fieldNameObj]);
       },
       sendEvent: function(category, action, label, value) {
-        var fieldNameObj = {
-          'hitType':       'event',  // Required
-          'eventCategory': category, // Required
-          'eventAction':   action    // Required
-        };
-
-        if (label != null) {
-          fieldNameObj.eventLabel = label;
-          if (value != null) {
-            fieldNameObj.eventValue = value;
-          }
-        }
-
-        tracker(namespace('send'), fieldNameObj);
+        logger('sendEvent', [category, action, label, value]);
       },
       trackPageView: function(path, fieldNameObj) {
-        fieldNameObj = fieldNameObj || {};
-
-        if (!path) {
-          var loc = window.location;
-          path = loc.hash ? loc.hash.substring(1) : (loc.pathname + loc.search);
-        }
-
-        tracker(namespace('send'), 'pageview', path, fieldNameObj);
+        logger('trackPageView', ['pageview', path, fieldNameObj]);
+      },
+      applyAppFields: function() {
+        logger('applyAppFields');
       }
     });
 
     return new Tracker();
-  },
-
-  trackerFun: trackerFun,
-  trackingNamespace: trackingNamespace
+  }
 };

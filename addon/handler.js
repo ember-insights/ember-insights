7fd6dcb616588cd3bf7de723efc9391c6942a97f
Zatvor <a.zatvornitskiy@gmail.com>
2015-03-30 23:57:03 +0300

    Encapsulate insights specific entities. A handler.js bahaves like a proxy, #123

--- addon/handler.js
+++ addon/handler.js
@@ -1,41 +1,13 @@
-function transitionHandler(data, tracker, settings) {
-  switch (settings.trackTransitionsAs) {
-    case 'event':
-      tracker.sendEvent(
-        'transition', JSON.stringify({ from: data.oldRouteName, to: data.routeName })
-      );
-      break;
-    case 'pageview':
-      tracker.trackPageView(data.url);
-      break;
-  }
-}
-
-function actionHandler(data, tracker, settings) {
-  settings = settings || {};
-  var actionLabel = data.actionArguments[0];
-  var actionValue = data.actionArguments[1];
-
-  tracker.sendEvent('action', data.actionName, actionLabel, actionValue);
-}
-
+import InsightsHandler from './insights-handler';
 
 export default {
   factory: function(settings) {
+    var insightsHandler = InsightsHandler.factory(settings);
+
     function defaultDispatcher(type, data, tracker) {
-      switch(type) {
-        case 'transition':
-          transitionHandler(data, tracker, settings);
-          break;
-        case 'action':
-          actionHandler(data, tracker, settings);
-          break;
-      }
+      insightsHandler(type, data, tracker);
     }
 
     return defaultDispatcher;
-  },
-
-  transitionHandler: transitionHandler,
-  actionHandler: actionHandler
+  }
 };
---
+++ tests/unit/handlers/insights-action-handler-test.js
@@ -0,0 +1,55 @@
+import DefaultHandler from 'ember-insights/insights-handler';
+import { it } from 'ember-mocha';
+
+
+describe('Insights Handler in context of #actionHandler',function() {
+  var handler = DefaultHandler.actionHandler;
+
+  it('sends action', function(done) {
+    var data = {
+      actionName: 'btnClick', actionArguments: []
+    };
+    var tracker = {
+      sendEvent: function(category, action, label, value) {
+        expect(category).to.equal('action');
+        expect(action).to.equal('btnClick');
+        done();
+      }
+    };
+
+    handler(data, tracker);
+  });
+
+  it('sends action with label', function(done) {
+    var data = {
+      actionName: 'btnClick', actionArguments: ['aLabel']
+    };
+    var tracker = {
+      sendEvent: function(category, action, label, value) {
+        expect(category).to.equal('action');
+        expect(action).to.equal('btnClick');
+        expect(label).to.equal('aLabel');
+        done();
+      }
+    };
+
+    handler(data, tracker);
+  });
+
+  it('sends action with label and value', function(done) {
+    var data = {
+      actionName: 'btnClick', actionArguments: ['aLabel', 'aValue']
+    };
+    var tracker = {
+      sendEvent: function(category, action, label, value) {
+        expect(category).to.equal('action');
+        expect(action).to.equal('btnClick');
+        expect(label).to.equal('aLabel');
+        expect(value).to.equal('aValue');
+        done();
+      }
+    };
+
+    handler(data, tracker);
+  });
+});

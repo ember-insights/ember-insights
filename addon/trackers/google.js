16167b4da2769c4e9ce58a60b32022fdc5f31db2
Zatvor <a.zatvornitskiy@gmail.com>
2015-04-21 17:09:56 +0300

    Rename 'GoogleTracker#tracker' getter to '#ga'

--- addon/trackers/google.js
+++ addon/trackers/google.js
@@ -11,29 +11,29 @@ function trackingNamespace(name) {
   return (action) => (name ? name + '.' : '') + action;
 }
 
-function setFields(tracker, namespace, fields) {
+function setFields(ga, namespace, fields) {
   for (var propName in fields) {
-    tracker(namespace('set'), propName, fields[propName]);
+    ga(namespace('set'), propName, fields[propName]);
   }
 }
 
 class GoogleTracker extends AbstractTracker {
   constructor(trackerOptions = {}) {
     super();
-    this.tracker  = () => trackerFun(trackerOptions.trackerFun || 'ga');
+    this.ga       = () => trackerFun(trackerOptions.trackerFun || 'ga');
     this.name     = trackingNamespace(trackerOptions.name || '');
 
     if (trackerOptions.fields) {
-      setFields(this.tracker(), this.name, trackerOptions.fields);
+      setFields(this.ga(), this.name, trackerOptions.fields);
     }
   }
 
   set(key, value) {
-    this.tracker()(this.name('set'), key, value);
+    this.ga()(this.name('set'), key, value);
   }
 
   send(fields = {}) {
-    this.tracker()(this.name('send'), fields);
+    this.ga()(this.name('send'), fields);
   }
 
   sendEvent(category, action, label, value) {
@@ -54,7 +54,7 @@ class GoogleTracker extends AbstractTracker {
       let loc = window.location;
       path = loc.hash ? loc.hash.substring(1) : (loc.pathname + loc.search);
     }
-    this.tracker()(this.name('send'), 'pageview', path, fields);
+    this.ga()(this.name('send'), 'pageview', path, fields);
   }
 }
 
